310d0e6d7536d5dca2fd3292fce9aeed1f24ea81
Robert <valentyne.robert@gmail.com>
2015-07-19 23:27:25 +0100

    updated item cards

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ app.controller('inventoryCtrl', ['$scope', '$http', function($scope, $http) {
         $http.jsonp("http://2.120.163.83:8080/api/v1/getInventory?sid=" + encodeURIComponent($scope.username) + "&callback=JSON_CALLBACK")
         .success(function(data){
             $scope.items = data;
+            console.log(data)
         }).error(function(){
             console.log("error")
         });
