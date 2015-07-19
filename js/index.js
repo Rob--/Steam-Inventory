7727cf41f36a7ae6dbec01d295df36e1b7c06f2b
Robert <valentyne.robert@gmail.com>
2015-07-19 22:26:11 +0100

    working requests

--- js/index.js
+++ js/index.js
@@ -1,37 +1,16 @@
 require('angular');
-var request = require('request');
-
-function evaluateUsername(username){
-    getInventory(username);
-}
-
-function getInventory(suffix){
-    console.log("..")
-/*    request('http://2.120.163.83:8080/api/v1/getInventory', function (error, response, body) {
-        console.log(".")
-        console.log(body)
-        console.log(error)
-        if (!error && response.statusCode == 200) {
-            console.log(body)
-        }
-    });*/
-
-}
 
 var app = angular.module('inventoryLoader', []);
 
 app.controller('inventoryCtrl', ['$scope', '$http', function($scope, $http) {
-    $scope.loadInventory = function(){
-        evaluateUsername($scope.username);
-        $http({method: "JSONP", url: "http://2.120.163.83:8080/api/v1/getInventory?sid=76561198090927398&callback=JSON_CALLBACK"}).success(function(data, status) {
-            console.log(data)
-            console.log(status)
-        }).error(function(data, status) {
-            console.log(data)
-            console.log(status)
-        });
+    $scope.items = [];
 
-        $http.jsonp("http://2.120.163.83:8080/api/v1/getInventory?sid=76561198090927398&callback=JSON_CALLBACK").success(function(data){
-        console.log(data)}).error(function(){console.log("error")})
+    $scope.loadInventory = function(){
+        $http.jsonp("http://2.120.163.83:8080/api/v1/getInventory?sid=" + encodeURIComponent($scope.username) + "&callback=JSON_CALLBACK")
+        .success(function(data){
+            $scope.items = data;
+        }).error(function(){
+            console.log("error")
+        });
     }
 }]);
