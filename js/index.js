b699f08e8a6af04420e1e8df76eb3878d5b5572f
Robert <valentyne.robert@gmail.com>
2015-07-19 18:04:49 +0100

    testing different http req method

--- js/index.js
+++ js/index.js
@@ -6,12 +6,21 @@ function evaluateUsername(username){
 }
 
 function getInventory(suffix){
+    console.log("..")
+/*    request('http://2.120.163.83:8080/api/v1/getInventory', function (error, response, body) {
+        console.log(".")
+        console.log(body)
+        console.log(error)
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+    });*/
 
 }
 
 var app = angular.module('inventoryLoader', []);
 
-app.controller('inventoryCtrl', function($scope) {
+app.controller('inventoryCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.loadInventory = function(){
         evaluateUsername($scope.username);
         $http({method: "JSONP", url: "http://2.120.163.83:8080/api/v1/getInventory"}).success(function(data, status) {
@@ -22,4 +31,4 @@ app.controller('inventoryCtrl', function($scope) {
             console.log(status)
         });
     }
-});
+}]);
