bc4bd696fe682b8796d29f2b573b013e2677504f
Robert <valentyne.robert@gmail.com>
2015-07-20 17:51:15 +0100

    added loading message during inventory fetch

--- js/index.js
+++ js/index.js
@@ -4,14 +4,18 @@ var app = angular.module('inventoryLoader', []);
 
 app.controller('inventoryCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.items = [];
+    $scope.columnSize = 2;
+    $scope.loading = false;
 
     $scope.loadInventory = function(){
+        $scope.loading = true;
         $http.jsonp("http://2.120.163.83:8080/api/v1/getInventory?sid=" + encodeURIComponent($scope.username) + "&callback=JSON_CALLBACK")
         .success(function(data){
+            $scope.loading = false;
             $scope.items = data;
-            console.log(data)
+            console.log("Successfully fteched inventory for " + $scope.username);
         }).error(function(){
-            console.log("error")
+            console.log("Error fetching inventory.")
         });
     }
 }]);
