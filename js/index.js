c27f9b022023ba5b003ce8edcb56ab54011ac222
Robert <valentyne.robert@gmail.com>
2015-07-23 12:21:56 +0100

    new vars & handling

--- js/index.js
+++ js/index.js
@@ -3,22 +3,19 @@ require('angular');
 var app = angular.module('inventoryLoader', ['ngAnimate']);
 
 app.controller('inventoryCtrl', ['$scope', '$http', function($scope, $http) {
-    $scope.items = [];
+    $scope.data = {items: [], total: 0}
     $scope.columnSize = 1;
     $scope.loading = false;
     $scope.error = false;
-    $scope.total = 0;
 
     $scope.loadInventory = function(){
         $scope.loading = true;
         $scope.error = false;
-        $scope.items = [];
-        $scope.total = 0;
+        $scope.data = {items: [], total: 0};
         $http.jsonp("http://2.120.163.83:8080/api/v1/getInventory?username=" + encodeURIComponent($scope.username) + "&callback=JSON_CALLBACK")
         .success(function(data){
             if(data.success){
-                $scope.items = data.items;
-                $scope.total = data.total;
+                $scope.data = data;
                 console.log("Successfully fetched inventory for " + $scope.username);
             } else {
                 $scope.error = true;
