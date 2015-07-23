92e478618c333286011448a5aa15a4cec7eb3036
Robert <valentyne.robert@gmail.com>
2015-07-23 11:55:10 +0100

    added support for pricings

--- js/index.js
+++ js/index.js
@@ -7,15 +7,18 @@ app.controller('inventoryCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.columnSize = 1;
     $scope.loading = false;
     $scope.error = false;
+    $scope.total = 0;
 
     $scope.loadInventory = function(){
         $scope.loading = true;
+        $scope.error = false;
         $scope.items = [];
+        $scope.total = 0;
         $http.jsonp("http://2.120.163.83:8080/api/v1/getInventory?username=" + encodeURIComponent($scope.username) + "&callback=JSON_CALLBACK")
         .success(function(data){
             if(data.success){
-                $scope.error = false;
                 $scope.items = data.items;
+                $scope.total = data.total;
                 console.log("Successfully fetched inventory for " + $scope.username);
             } else {
                 $scope.error = true;
