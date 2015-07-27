8f602e6d6dd8259d4ce525005627e261aa6396b8
Robert <valentyne.robert@gmail.com>
2015-07-27 16:25:42 +0100

    .

--- js/index.js
+++ js/index.js
@@ -80,6 +80,18 @@ app.controller('inventoryCtrl', ['$scope', '$http', '$sce', function($scope, $ht
         }
     }]
 )
-.directive('repeatDirective', function() {
+/*.directive('repeatDirective', function() {
   return function(scope) { if(scope.$last) scope.$emit('initialiseTooltips'); };
-})
+})*/
+.directive('repeatDirective', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            if (scope.$last === true) {
+                $timeout(function () {
+                    scope.$emit('initialiseTooltips');
+                });
+            }
+        }
+    }
+});
