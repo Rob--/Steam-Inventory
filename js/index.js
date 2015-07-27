1ad67abb4bfa6c82440be677fa3eb70aa0f99665
Robert <valentyne.robert@gmail.com>
2015-07-27 16:01:19 +0100

    added schema support

--- js/index.js
+++ js/index.js
@@ -36,7 +36,7 @@ app.controller('inventoryCtrl', ['$scope', '$http', '$sce', function($scope, $ht
             $scope.error = true;
             console.log("Error fetching inventory.")
         });
-    };$('.tooltipped').tooltip({delay: 1000});
+    };
 
     $scope.updateCurrency = function(currency){
         $scope.currency.abbr = $scope.currencies[currency][0];
@@ -45,7 +45,28 @@ app.controller('inventoryCtrl', ['$scope', '$http', '$sce', function($scope, $ht
     }
 
     $scope.$on('initialiseTooltips', function(event){
-        alert("testing final")
+        $('.tooltipped').each(function(){
+
+            //"<p style='font-family:Roboto; margin:5px'>AWP | Dragon Lore (Field-Tested)</p><hr>Float: 0.03242424<hr><img width='70px' height='50px' src='https://steamcdn-a.akamaihd.net/apps/730/icons/econ/stickers/eslkatowice2015/fnatic_holo.958b36604c0485b89dfabe71294f04b25ada7bb4.png'><img width='70px' height='50px' src='https://steamcdn-a.akamaihd.net/apps/730/icons/econ/stickers/stickers2/crown_foil.77c38fe60426ee084fd5c8fec0c680c342e05743.png'><hr><a class='waves-effect waves-light btn' style='background-color:#fff'>Inspect</a><hr>Karambit (Knife), Covert<hr>The Vanguard Collection"
+
+            var item = $scope.data.items[Number($(this).attr('id').replace("item_", ""))];
+            var data = "<p style='font-family:Roboto; margin:5px'>";
+            data += item.hash_name;
+            data += "</p><hr><p style='style='font-family:Roboto'>Float:</p> ";
+            data += String(item.schema.float);
+            data += "<hr>";
+            for(var i = 0; i < item.stickers.images.length; i++){
+                data += "<img width='70px' height='50px' src='" + item.stickers.images[i] + "'>";
+            }
+            data += "<hr><a class='waves-effect waves-light btn' href='" + item.inspect + "'>Inspect</a><hr>";
+            data += item.weapon + "(" + item.type + "), " + item.rarity.rarity + "<hr>";
+            data += item.collection;
+
+            $(this).attr("data-tooltip", data);
+
+        });
+
+        $('.tooltipped').tooltip();
     });
 }])
 /* http://stackoverflow.com/a/25842874/2536231 */
@@ -55,8 +76,7 @@ app.controller('inventoryCtrl', ['$scope', '$http', '$sce', function($scope, $ht
             return $sce.trustAsHtml(curr + " " + input);
         }
     }]
-).directive('repeatDirective', function() {
-  return function(scope) {
-    if(scope.$last) scope.$emit('initialiseTooltips');
-  };
+)
+.directive('repeatDirective', function() {
+  return function(scope) { if(scope.$last) scope.$emit('initialiseTooltips'); };
 })
